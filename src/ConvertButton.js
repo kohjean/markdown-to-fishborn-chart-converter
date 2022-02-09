0255e843eed55676c7cea91212dc43b6ae9d41d1
kohjean <58461459+kohjean@users.noreply.github.com>
2022-02-09 20:04:12 +0900

    beatify code

--- src/ConvertButton.js
+++ src/ConvertButton.js
@@ -1,7 +1,7 @@
 import Button from '@mui/material/Button';
 import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
 
-export const ConvertButton = ({target}) => {
+export const ConvertButton = ({ target }) => {
   return (
     <>
       <Button
