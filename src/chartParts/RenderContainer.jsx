79ea4f0b939289dd2a146ba56c37756396da527f
kohjean <58461459+kohjean@users.noreply.github.com>
2022-02-14 15:37:43 +0900

    メインテーマのコンポーネント用にCSSを追記した

--- src/chartParts/RenderContainer.jsx
+++ src/chartParts/RenderContainer.jsx
@@ -3,19 +3,26 @@ import { Spine } from './Spine';
 import { RecursiveComponent } from './RecursiveComponent';
 
 export const RenderContainer = ({ root }) => {
-  const { text, depth, children, id } = root;
+  const { text, depth, children } = root;
   return (
     <SDiv className="container">
       <Spine />
-      <div data-depth={depth} id={`no_${id}`}>
-        {text}
-      </div>
       {children.map((elem) => (
         <RecursiveComponent props={elem} />
+      <MainPloblem data-depth={depth}>{text}</MainPloblem>
       ))}
     </SDiv>
   );
 };
+
+const MainPloblem = styled.div`
+  top: calc(50%);
+  left: calc(100% - 7rem);
+  width: 7rem;
+  border: 1px solid #000;
+  transform: translateY(-50%);
+  text-align: center;
+`;
 
 const SDiv = styled.div`
   background-color: #fff;
