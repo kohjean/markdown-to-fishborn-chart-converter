46e752343c46d840f8d9c358c9046a92cf041d3f
kohjean <58461459+kohjean@users.noreply.github.com>
2022-02-16 14:23:01 +0900

    Add: DOMから画像を生成してダウンロードできるようにした

--- src/ConvertResult.js
+++ src/ConvertResult.js
@@ -1,6 +1,8 @@
 import { useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from '@emotion/styled';
+import domtoimage from 'dom-to-image';
+
 import { FishBornChart } from './FishBornChart';
 import { CancelButton } from './CancelButton';
 import { DownloadButton } from './DownloadButton';
@@ -14,12 +16,35 @@ const Flex = styled.div`
 export const ConvertResult = () => {
   const params = useParams();
   const chartImg = useRef();
+
+  const download = () => {
+    // domからイメージを生成
+    domtoimage
+      .toPng(chartImg.current)
+      .then((base64) => {
+        const bin = window.atob(base64.replace(/^.+,/, ''));
+        const buffer = new Uint8Array(bin.length);
+        for (let i = 0; i < bin.length; i++) {
+          buffer[i] = bin.charCodeAt(i);
+        }
+        const blob = new Blob([buffer.buffer], { type: 'image/png' });
+        // ダウンロード処理
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = `fb-chart.png`;
+        link.click();
+      })
+      .catch((e) => {
+        console.error('You got a error: ' + e);
+      });
+  };
+
   return (
     <>
       <FishBornChart markdown={params.markdown} ref={chartImg} />
       <Flex>
         <CancelButton />
-        <DownloadButton />
+        <DownloadButton handler={download} />
         <div />
       </Flex>
     </>
