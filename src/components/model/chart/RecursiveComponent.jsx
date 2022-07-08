46535a652aae82321e94ea0784f99f6d9720cfbf
kohjean <58461459+kohjean@users.noreply.github.com>
2022-07-08 17:44:40 +0900

    Change: emotion/styledをemotion/reactのString Stylesに変更した
    packageはMUIが依存しているので削除できなかった

--- src/components/model/chart/RecursiveComponent.jsx
+++ src/components/model/chart/RecursiveComponent.jsx
@@ -1,13 +1,13 @@
-import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 export const RecursiveComponent = ({ props, idx, branchIdx }) => {
   const { text, depth, children } = props;
   const isUpperBranch = (idx) => idx % 2 === 0;
   return (
     <>
-      <Node
+      <div
+        css={nodeStyle({ idx: idx, isUpper: isUpperBranch(branchIdx) })}
         data-depth={depth}
-        theme={{ myIndex: idx, isUpper: isUpperBranch(branchIdx) }}
       >
         {text}
         {children.length > 0 &&
@@ -19,40 +19,44 @@ export const RecursiveComponent = ({ props, idx, branchIdx }) => {
               branchIdx={branchIdx}
             />
           ))}
-      </Node>
+      </div>
     </>
   );
 };
 
-const Node = styled.div`
+const nodeStyle = ({ idx, isUpper }) => css`
   &[data-depth='1'] {
     width: 250px;
     border-bottom: solid 1px #000;
     // 上下で異なる
-    top: ${({ theme }) =>
-      `calc(${theme.isUpper ? '122px + 0.5rem' : '375px - 1.5rem + 2px'})`};
-    transform: ${({ theme }) =>
-      `${theme.isUpper ? 'rotate(60deg) translateY(-50%)' : 'rotate(120deg)'}`};
+    ${isUpper
+      ? `
+        top: calc(122px + 0.5rem);
+        transform: rotate(60deg) translateY(-50%);
+    `
+      : `
+        top: calc(375px - 1.5rem + 2px);
+        transform: rotate(120deg);
+      `}
     // インデックスで異なる
-    left: ${({ theme }) => `calc(100% - 12rem * ${theme.myIndex * 0.5 + 2})`};
+    left: calc(100% - 12rem * ${idx * 0.5 + 2});
   }
   &[data-depth='2'] {
     font-size: 0.8rem;
     top: calc((98px + 1.5rem) / 2);
     // 領域(子要素(数, 高さ))で異なる
     width: 96px;
-    // 上下で異なる
-    transform: ${({ theme }) =>
-      theme.isUpper ? 'rotate(-60deg)' : 'rotate(-120deg)'};
-    ${({ theme }) =>
-      theme.isUpper
-        ? 'border-top: 1px solid #000;'
-        : 'border-bottom: 1px solid #000;'};
-    // インデックスで異なる
-    left: ${({ theme }) =>
-      theme.isUpper
-        ? `${185 - (theme.myIndex + 1) * 54}px`
-        : `${-30 + (theme.myIndex + 1) * 50}px`};
+    ${isUpper
+      ? `
+        transform: rotate(-60deg);
+        border-top: 1px solid #000;
+        left: calc(185px - (${idx + 1} * 54px));
+    `
+      : `
+        transform: rotate(-120deg);
+        border-bottom: 1px solid #000;
+        left: calc(-30px + (${idx + 1} * 50px));
+      `}
   }
   &[data-depth='3'] {
     font-size: 0.6rem;
@@ -60,11 +64,18 @@ const Node = styled.div`
     border-bottom: 1px solid #000;
     width: 64px;
     // 上下で異なる
-    text-align: ${({ theme }) => (theme.isUpper ? 'right' : 'left')};
-    top: ${({ theme }) => (theme.isUpper ? '-40px' : '43px')};
-    transform: ${({ theme }) =>
-      theme.isUpper ? 'rotate(60deg)' : 'rotate(120deg)'};
+    ${isUpper
+      ? `
+      text-align: right;
+      top: -40px;
+      transform: rotate(60deg);
+    `
+      : `
+      text-align: left;
+      top: 43px;
+      transform: rotate(120deg);
+    `}
     // インデックスで異なる
-    left: ${({ theme }) => `${36 - theme.myIndex * 32}px`};
+    left: calc(36px - ${idx * 32}px)
   }
 `;
